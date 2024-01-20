cc9347133403fad8e81d183f78e0b15c036f3b0f
MMolik <molikos@o2.pl>
2024-01-20 19:36:08 +0100

    Task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const widget = document.querySelector(".widget");
+const colorSpan = document.querySelector(".color");
+
+button.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  widget.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
